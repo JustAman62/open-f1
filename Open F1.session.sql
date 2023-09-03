65cddb90c532ad54d093fb6d007dc667c5d09292
BrownKnight <amanrk@hotmail.co.uk>
2023-09-03 16:27:41 +0100

    Add grafana dashboard

--- Open F1.session.sql
+++ Open F1.session.sql
@@ -1,7 +1,6 @@
 
-SELECT RAW_DATA FROM RAW_DATA_POINTS
-where DATA_TYPE = 'TimingData'
-order by ID desc
- 
-
-
+SELECT * FROM RAW_DATA_POINTS
+--where EventType = 'TimingData'
+where EventType not in ('Position.z', 'CarData.z')
+--where EventType in ('WeatherData', 'RaceControlMessages')
+order by LOGD_TS desc
