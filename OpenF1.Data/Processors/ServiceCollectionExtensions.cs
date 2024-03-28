befb0c84c9675fa747e968d56b64a38939abc871
BrownKnight <amanrk@hotmail.co.uk>
2024-03-28 20:06:26 +0000

    Automatically register IProcessor implementations

--- OpenF1.Data/Processors/ServiceCollectionExtensions.cs
+++ OpenF1.Data/Processors/ServiceCollectionExtensions.cs
@@ -6,24 +6,30 @@ public static partial class ServiceCollectionExtensions
 {
     public static IServiceCollection AddLiveTimingProcessors(this IServiceCollection collection)
     {
-        collection
-            .AddProcessor<TrackStatusDataPoint, TrackStatusProcessor>()
-            .AddProcessor<HeartbeatDataPoint, HeartbeatProcessor>()
-            .AddProcessor<RaceControlMessageDataPoint, RaceControlMessageProcessor>()
-            .AddProcessor<TimingDataPoint, TimingDataProcessor>()
-            .AddProcessor<TimingAppDataPoint, TimingAppDataProcessor>()
-            .AddProcessor<LapCountDataPoint, LapCountProcessor>()
-            .AddProcessor<WeatherDataPoint, WeatherProcessor>()
-            .AddProcessor<DriverListDataPoint, DriverListProcessor>();
-            
+        var processorTypes = typeof(IProcessor)
+            .Assembly.GetTypes()
+            .Where(x => x.IsClass && x.IsAssignableTo(typeof(IProcessor)));
+
+        foreach (var processorType in processorTypes)
+        {
+            var dataPointType = processorType
+                .GetInterfaces()
+                .First(x =>
+                    x.IsGenericType
+                    && x.GenericTypeArguments.Length > 0
+                    && x.GenericTypeArguments.First().IsAssignableTo(typeof(ILiveTimingDataPoint))
+                )
+                .GenericTypeArguments.First();
+
+            collection
+                .AddSingleton(typeof(IProcessor), x => x.GetRequiredService(processorType))
+                .AddSingleton(
+                    typeof(IProcessor<>).MakeGenericType(dataPointType),
+                    x => x.GetRequiredService(processorType)
+                )
+                .AddSingleton(processorType, processorType);
+        }
+
         return collection;
     }
-
-    private static IServiceCollection AddProcessor<TDataPoint, TProcessor>(
-        this IServiceCollection services
-    ) where TProcessor: class, IProcessor<TDataPoint> =>
-        services
-            .AddSingleton<IProcessor>(x => x.GetRequiredService<TProcessor>())
-            .AddSingleton<IProcessor<TDataPoint>>(x => x.GetRequiredService<TProcessor>())
-            .AddSingleton<TProcessor>();
 }
