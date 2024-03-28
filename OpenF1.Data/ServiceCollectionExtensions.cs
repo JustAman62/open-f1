d2052230597f1afdbe1493d446008f9bb9e67c5b
BrownKnight <amanrk@hotmail.co.uk>
2024-03-28 20:52:20 +0000

    Improve docs on service collection extensions

--- OpenF1.Data/ServiceCollectionExtensions.cs
+++ OpenF1.Data/ServiceCollectionExtensions.cs
@@ -7,6 +7,14 @@ namespace OpenF1.Data;
 
 public static partial class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds the services required to support processing live timing data.
+    /// A <see cref="ILiveTimingClient"/> is registered to support ingesting live timing data from the live feed.
+    /// A <see cref="IJsonTimingClient"/> is registered to support ingesting previously recorded timing data from JSON files.
+    /// </summary>
+    /// <param name="collection">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configuration">The <see cref="IConfiguration"/> to bind <see cref="LiveTimingOptions"/></param>
+    /// <returns>The same <paramref name="collection"/> to support chaining.</returns>
     public static IServiceCollection AddLiveTiming(this IServiceCollection collection, IConfiguration configuration)
     {
         collection
