b362b32cebf44f02ca6ffe58fba18a4be80dc0fe
BrownKnight <amanrk@hotmail.co.uk>
2024-07-01 19:42:52 +0100

    Don;t overwrite circuit info when mapping

--- OpenF1.Data/AutoMapper/SessionInfoDataPointConfiguration.cs
+++ OpenF1.Data/AutoMapper/SessionInfoDataPointConfiguration.cs
@@ -6,5 +6,6 @@ public class SessionInfoDataPointConfiguration : Profile
 {
     public SessionInfoDataPointConfiguration() =>
         CreateMap<SessionInfoDataPoint, SessionInfoDataPoint>()
+            .ForMember(x => x.CircuitPoints, opts => opts.Ignore())
             .ForAllMembers(opts => opts.Condition((_, _, member) => member != null));
 }
