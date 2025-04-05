0c1036b77648d1c9442a60bfd89379132200c81b
JustAman62 <amanrk@hotmail.co.uk>
2025-04-05 08:02:25 +0100

    Handle ints given as floats from the MultiViewer API for circuit info

--- OpenF1.Data/Processors/SessionInfoProcessor.cs
+++ OpenF1.Data/Processors/SessionInfoProcessor.cs
@@ -1,4 +1,6 @@
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using AutoMapper;
 using Microsoft.Extensions.Logging;
 
@@ -8,6 +10,13 @@ public class SessionInfoProcessor(IMapper mapper, ILogger<SessionInfoProcessor> 
     : ProcessorBase<SessionInfoDataPoint>(mapper)
 {
     private Task? _loadCircuitTask = null;
+    private JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        NumberHandling =
+            JsonNumberHandling.AllowNamedFloatingPointLiterals
+            | JsonNumberHandling.AllowReadingFromString,
+        Converters = { new IntJsonConverter() },
+    };
 
     public override void Process(SessionInfoDataPoint data)
     {
@@ -40,7 +49,7 @@ public class SessionInfoProcessor(IMapper mapper, ILogger<SessionInfoProcessor> 
             var url =
                 $"https://api.multiviewer.app/api/v1/circuits/{circuitKey}/{DateTimeOffset.UtcNow.Year}";
             var circuitInfo = await httpClient
-                .GetFromJsonAsync<CircuitInfoResponse>(url)
+                .GetFromJsonAsync<CircuitInfoResponse>(url, _jsonSerializerOptions)
                 .ConfigureAwait(false);
 
             Latest.CircuitPoints = circuitInfo!.X.Zip(circuitInfo.Y).ToList();
@@ -65,4 +74,22 @@ public class SessionInfoProcessor(IMapper mapper, ILogger<SessionInfoProcessor> 
     private sealed record TrackCornerResponse(int Number, TrackPositionResponse TrackPosition);
 
     private sealed record TrackPositionResponse(float X, float Y);
+}
+
+/// <summary>
+/// A JsonConverter that handles ints that are formatted as floats (e.g. 1023.0).
+// If the value is a real float (e.g. 1023.04) then this converter will delegate
+// to <see cref="Utf8JsonReader.GetInt32()"/> which will throw a <see cref="JsonException"/>.
+/// </summary>
+internal class IntJsonConverter : JsonConverter<int>
+{
+    public override int Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options)
+    {
+        var valDouble = reader.GetDouble();
+        var valInt = (int)valDouble;
+        return valDouble == valInt ? valInt : reader.GetInt32();
+    }
+
+    public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options) =>
+        writer.WriteNumberValue(value);
 }
