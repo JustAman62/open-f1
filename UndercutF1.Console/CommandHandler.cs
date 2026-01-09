504dccaa0a547205e810121b79a6dbb79d0aa6fd
JustAman62 <amanrk@hotmail.co.uk>
2026-01-09 19:30:02 +0000

    Format

--- UndercutF1.Console/CommandHandler.cs
+++ UndercutF1.Console/CommandHandler.cs
@@ -236,7 +236,10 @@ public static partial class CommandHandler
         }
         if (preferFfmpeg is not null)
         {
-            commandLineOpts.Add(nameof(ConsoleOptions.PreferFfmpegPlayback), preferFfmpeg.ToString());
+            commandLineOpts.Add(
+                nameof(ConsoleOptions.PreferFfmpegPlayback),
+                preferFfmpeg.ToString()
+            );
         }
         if (preventDisplaySleep is not null)
         {
@@ -254,7 +257,11 @@ public static partial class CommandHandler
         }
 
         _ = builder
-            .Configuration.AddJsonFile(ConsoleOptions.ConfigFilePath, optional: true, reloadOnChange: true)
+            .Configuration.AddJsonFile(
+                ConsoleOptions.ConfigFilePath,
+                optional: true,
+                reloadOnChange: true
+            )
             .AddEnvironmentVariables("UNDERCUTF1_")
             .AddInMemoryCollection(commandLineOpts);
 
